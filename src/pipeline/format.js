40edf186e5398154f83932d2c8633ddb44a3fb48
Mash Ibtesum <mashnoon33@gmail.com>
2020-01-03 00:18:15 -0500

    Hardcoded central time as the default timezone

--- src/pipeline/format.js
+++ src/pipeline/format.js
@@ -1,93 +1,91 @@
-import {
-    setAlarm,
-    setContact,
-    setOrganizer,
-    formatDate,
-    setDescription,
-    setSummary,
-    setGeolocation,
-    formatDuration,
-    foldLine
-} from '../utils'
-import isEqual from 'lodash/isEqual'
+"use strict";
 
-export default function formatEvent(attributes = {}) {
-  const {
-    title,
-    productId,
-    method,
-    uid,
-    sequence,
-    timestamp,
-    start,
-    startType,
-    startInputType,
-    startOutputType,
-    duration,
-    end,
-    endInputType,
-    endOutputType,
-    description,
-    url,
-    geo,
-    location,
-    status,
-    categories,
-    organizer,
-    attendees,
-    alarms,
-    recurrenceRule,
-    busyStatus
-  } = attributes
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = formatEvent;
 
-  let icsFormat = ''
-  icsFormat += 'BEGIN:VCALENDAR\r\n'
-  icsFormat += 'VERSION:2.0\r\n'
-  icsFormat += 'CALSCALE:GREGORIAN\r\n'
-  icsFormat += foldLine(`PRODID:${productId}`) + '\r\n'
-  icsFormat += foldLine(`METHOD:${method}`) + '\r\n'
-  icsFormat += `X-PUBLISHED-TTL:PT1H\r\n`
-  icsFormat += 'BEGIN:VEVENT\r\n'
-  icsFormat += `UID:${uid}\r\n`
-  icsFormat +=  foldLine(`SUMMARY:${title ? setSummary(title) : title}`) + '\r\n'
-  icsFormat += `DTSTAMP:${timestamp}\r\n`
+var _utils = require("../utils");
 
-  // All day events like anniversaries must be specified as VALUE type DATE
-  icsFormat += `DTSTART${start && start.length == 3 ? ";VALUE=DATE" : ""}:${formatDate(start, startOutputType || startType, startInputType)}\r\n`
+var _isEqual = _interopRequireDefault(require("lodash/isEqual"));
 
-  // End is not required for all day events on single days (like anniversaries)
-  if (!(isEqual(start, end) && end && end.length == 3)) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function formatEvent() {
+  var attributes = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var title = attributes.title,
+      productId = attributes.productId,
+      method = attributes.method,
+      uid = attributes.uid,
+      sequence = attributes.sequence,
+      timestamp = attributes.timestamp,
+      start = attributes.start,
+      startType = attributes.startType,
+      startInputType = attributes.startInputType,
+      startOutputType = attributes.startOutputType,
+      duration = attributes.duration,
+      end = attributes.end,
+      endInputType = attributes.endInputType,
+      endOutputType = attributes.endOutputType,
+      description = attributes.description,
+      url = attributes.url,
+      geo = attributes.geo,
+      location = attributes.location,
+      status = attributes.status,
+      categories = attributes.categories,
+      organizer = attributes.organizer,
+      attendees = attributes.attendees,
+      alarms = attributes.alarms,
+      recurrenceRule = attributes.recurrenceRule,
+      busyStatus = attributes.busyStatus;
+  var icsFormat = '';
+  icsFormat += 'BEGIN:VCALENDAR\r\n';
+  icsFormat += 'VERSION:2.0\r\n';
+  icsFormat += 'CALSCALE:GREGORIAN\r\n';
+  icsFormat += (0, _utils.foldLine)("PRODID:".concat(productId)) + '\r\n';
+  icsFormat += (0, _utils.foldLine)("METHOD:".concat(method)) + '\r\n';
+  icsFormat += "X-PUBLISHED-TTL:PT1H\r\n";
+  icsFormat += 'BEGIN:VTIMEZONE\r\n' + 'TZID:America/Chicago\r\n' + 'BEGIN:DAYLIGHT\r\n' + 'TZOFFSETFROM:-0600\r\n' + 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU\r\n' +
+  'DTSTART:20070311T020000\r\n' + 'TZNAME:CDT\r\n' + 'TZOFFSETTO:-0500\r\n' + 'END:DAYLIGHT\r\n' + 'BEGIN:STANDARD\r\n' + 'TZOFFSETFROM:-0500\r\n' + 'RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU\r\n' + 
+  'DTSTART:20071104T020000\r\n' + 'TZNAME:CST\r\n' + 'TZOFFSETTO:-0600\r\n' + 'END:STANDARD\r\n' + 'END:VTIMEZONE\r\n'
+  icsFormat += 'BEGIN:VEVENT\r\n';
+  icsFormat += "UID:".concat(uid, "\r\n");
+  icsFormat += (0, _utils.foldLine)("SUMMARY:".concat(title ? (0, _utils.setSummary)(title) : title)) + '\r\n';
+  icsFormat += "DTSTAMP:".concat(timestamp, "\r\n"); // All day events like anniversaries must be specified as VALUE type DATE
+
+  icsFormat += "DTSTART;TZID=America/Chicago".concat(start && start.length == 3 ? ";VALUE=DATE" : "", ":").concat((0, _utils.formatDate)(start, startOutputType || startType, startInputType), "\r\n"); // End is not required for all day events on single days (like anniversaries)
+
+  if (!((0, _isEqual["default"])(start, end) && end && end.length == 3)) {
     if (end) {
-      icsFormat += `DTEND${end.length == 3 ? ";VALUE=DATE" : ""}:${formatDate(end, endOutputType || startOutputType || startType, endInputType || startInputType)}\r\n`;
+      icsFormat += "DTEND;TZID=America/Chicago".concat(end.length == 3 ? ";VALUE=DATE" : "", ":").concat((0, _utils.formatDate)(end, endOutputType || startOutputType || startType, endInputType || startInputType), "\r\n");
     }
   }
 
-  icsFormat += sequence ? (`SEQUENCE:${sequence}\r\n`) : ''
-  icsFormat += description ? (foldLine(`DESCRIPTION:${setDescription(description)}`) + '\r\n') : ''
-  icsFormat += url ? (foldLine(`URL:${url}`) + '\r\n') : ''
-  icsFormat += geo ? (foldLine(`GEO:${setGeolocation(geo)}`) + '\r\n') : ''
-  icsFormat += location ? (foldLine(`LOCATION:${location}`) + '\r\n') : ''
-  icsFormat += status ? (foldLine(`STATUS:${status}`) + '\r\n') : ''
-  icsFormat += categories ? (foldLine(`CATEGORIES:${categories}`) + '\r\n') : ''
-  icsFormat += organizer ? (foldLine(`ORGANIZER;${setOrganizer(organizer)}`) + '\r\n') : ''
-  icsFormat += busyStatus ? (foldLine(`X-MICROSOFT-CDO-BUSYSTATUS:${busyStatus}`) + '\r\n') : ''
+  icsFormat += sequence ? "SEQUENCE:".concat(sequence, "\r\n") : '';
+  icsFormat += description ? (0, _utils.foldLine)("DESCRIPTION:".concat((0, _utils.setDescription)(description))) + '\r\n' : '';
+  icsFormat += url ? (0, _utils.foldLine)("URL:".concat(url)) + '\r\n' : '';
+  icsFormat += geo ? (0, _utils.foldLine)("GEO:".concat((0, _utils.setGeolocation)(geo))) + '\r\n' : '';
+  icsFormat += location ? (0, _utils.foldLine)("LOCATION:".concat(location)) + '\r\n' : '';
+  icsFormat += status ? (0, _utils.foldLine)("STATUS:".concat(status)) + '\r\n' : '';
+  icsFormat += categories ? (0, _utils.foldLine)("CATEGORIES:".concat(categories)) + '\r\n' : '';
+  icsFormat += organizer ? (0, _utils.foldLine)("ORGANIZER;".concat((0, _utils.setOrganizer)(organizer))) + '\r\n' : '';
+  icsFormat += busyStatus ? (0, _utils.foldLine)("X-MICROSOFT-CDO-BUSYSTATUS:".concat(busyStatus)) + '\r\n' : '';
 
   if (attendees) {
     attendees.map(function (attendee) {
-      icsFormat += foldLine(`ATTENDEE;${setContact(attendee)}`) + '\r\n'
-    })
+      icsFormat += (0, _utils.foldLine)("ATTENDEE;".concat((0, _utils.setContact)(attendee))) + '\r\n';
+    });
   }
-  
+
   if (alarms) {
     alarms.map(function (alarm) {
-      icsFormat += setAlarm(alarm)
-    })
+      icsFormat += (0, _utils.setAlarm)(alarm);
+    });
   }
 
-  icsFormat += recurrenceRule ? `RRULE:${recurrenceRule}\r\n` : ''
-  icsFormat += duration ? `DURATION:${formatDuration(duration)}\r\n` : ''
-  icsFormat += `END:VEVENT\r\n`
-  icsFormat += `END:VCALENDAR\r\n`
-
-  return icsFormat
+  icsFormat += recurrenceRule ? "RRULE:".concat(recurrenceRule, "\r\n") : '';
+  icsFormat += duration ? "DURATION:".concat((0, _utils.formatDuration)(duration), "\r\n") : '';
+  icsFormat += "END:VEVENT\r\n";
+  icsFormat += "END:VCALENDAR\r\n";
+  return icsFormat;
 }
